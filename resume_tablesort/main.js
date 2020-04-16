734d2e2e03fe7fcb8ed6355a9d993a44e07f25e0
Aydex <maksimrozette@gmail.com>
2020-04-16 18:59:25 +0300

    code preview and fix bags

--- resume_tablesort/main.js
+++ resume_tablesort/main.js
@@ -1,30 +1,42 @@
-let ths = document.getElementsByTagName("th");
-let trs = document.getElementsByTagName("tr");
-let tds = document.getElementsByTagName("td");
-let f = tds.length;
-let min = f-1;
-ths[0].addEventListener("click", Sort);
+
+// Комментарии пока будут на русском языке, позже пофикшу это.
+// Присваиваем переменным значения th и td в таблице
+let th_value = document.getElementsByTagName("th");
+let td_value = document.getElementsByTagName("td");
+// Передаем длину массива со значениями td переменной tdLength
+let tdLength = td_value.length;
+// Далее присваиваем последнее значение массива td переменной last_td_value
+let last_td_value = tdLength-1; 
+// Вешаем простой обработчик событий и при клике задействуем функцию Sort для вычисления и сортировки данных
+th_value[0].addEventListener("click", Sort);
+
+// Создаем функцию Sort 
 function Sort() {
-  var g = [];
-  for(let i = 0; i < f; i++){
-    var a = tds[i].innerHTML;
-    g.push(a);
-    if(i == f-1){
-         console.log(g);
-      if(g[0]>g[min]){
-        g.sort();
-        for(let j = 0; j < f; j++){
-          let b = g[j];
-          tds[j].innerHTML = g[j];
+  
+  let sort_arr_td = []; // Создаем пустой массив для сохранения в него данных из цикла
+  
+  for(let i = 0; i < tdLength; i++){
+// Сохраняем в переменную данные в соответствии с индексом их расположения в массиве td...
+    let a = td_value[i].innerHTML; 
+// Записываем эти данные в пустой массив
+    sort_arr_td.push(a);
+// Если i будет равным последнему "индексу" массива tdLength 
+    if(i == tdLength-1){
+// Если первый эелемент будет больше последнего - выполнить сортировку и вывести в порядке от меньшего к большему
+      if(sort_arr_td[0]>sort_arr_td[last_td_value]){
+          sort_arr_td.sort();//Сортировка
+        for(let j = 0; j < tdLength; j++){
+          td_value[j].innerHTML = sort_arr_td[j];
         }
-      }else if(g[0]<g[min]){
-        g.sort();
-        g.reverse();
-      for(let j = 0; j < f; j++){
-        let b = g[j];
-        tds[j].innerHTML = g[j];
       }
-      }else{console.log("Что то пошло не так");}
+// Если первый эелемент будет меньше последнего - выполнить сортировку и вывести в обратном порядке от большего к меньшему
+      else if(sort_arr_td[0]<sort_arr_td[last_td_value]){
+        sort_arr_td.sort(); //Сортировка
+        sort_arr_td.reverse(); //Реверсия"разворот" массива
+        for(let j = 0; j < tdLength; j++){
+          td_value[j].innerHTML = sort_arr_td[j];
+        }
+      }else{console.log("Maybe you can fix this? Error :D");}// Вывести ошибку если условия не имеют значения true "то есть если ни один из вариантов не работает"...
     }
   }
 }
