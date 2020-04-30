4e31d86a11d35f3783fa033426c46cbe24c12deb
Maksim Rengevich <maksimrozette@gmail.com>
2020-04-30 23:51:47 +0300

    ц

--- maketone/js/scroll.js
+++ maketone/js/scroll.js
@@ -9,7 +9,37 @@
 		//узнаем высоту от начала страницы до блока на который ссылается якорь
 			top = $(id).offset().top;
 		
-		//анимируем переход на расстояние - top за 1500 мс
-		$('body,html').animate({scrollTop: top}, 1500);
+		//анимируем переход на расстояние - top за 750 мс
+		$('body,html').animate({scrollTop: top}, 750);
+	});
+});
+$(document).ready(function(){
+	$("#arrowsss").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 750 мс
+		$('body,html').animate({scrollTop: top}, 750);
+	});
+});
+$(document).ready(function(){
+	$("#homsss").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 750 мс
+		$('body,html').animate({scrollTop: top}, 7520);
 	});
 });
