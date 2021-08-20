da39f2d138ed263cb4023b4c96e0bafa20b8f3ad
marevich <maksimrozette@gmail.com>
2021-08-20 14:16:27 +0300

    tree commit

--- rezume/main.php
+++
@@ -1,8 +0,0 @@
-<?php
-$name = $_POST['username'];
-$surname = $_POST['email'];
-$message = $_POST['message'];
-$file = fopen("file.txt","at");
-fwrite($file,"\n $name:$surname:$message \n");
-fclose($file);
-?>
--- store/js/add-to-cart.js
+++
@@ -1,8 +0,0 @@
-
-function addToCart() {
-    var link = document.getElementsByClassName("add");
-    var lengths = link.length;
-    var cart = document.getElementsByClassName("cart__number")[0];
-       cart.innerHTML = ++cart.innerHTML;
-       console.log(cart.innerHTML);
-}
