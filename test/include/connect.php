aa10cfa72ee48516269ea9ecdc106d99cf39eb41
Max <36448737+maksimtenor@users.noreply.github.com>
2021-11-11 14:36:21 +0300

    ddd

--- test/include/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//myscqli_connect
-    error_reporting(0);
-    $connect = mysqli_connect('localhost', 'root', '', 'test');
-
-    if(!$connect) {
-        die('Error connect to DataBase');
-    }
